26082b266efcf09f56bd2c081097b911d88fd8ff
Sabir Hassan <sabirbk06@gmail.com>
2024-06-14 15:04:42 +0500

    chore: installed mui library and added theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
+import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
+import { theme } from "./theme"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <CssVarsProvider theme={theme}>
+      <App />
+    </CssVarsProvider>
+  </React.StrictMode>
 )
