aea73841c5c218847c143ea022c0e095563aa345
Sabir Hassan <sabirbk06@gmail.com>
2024-06-24 19:37:03 +0500

    clear previous files

--- src/store/drawer/action.ts
+++
@@ -1,7 +0,0 @@
-import * as ActionTypes from "../actionTypes"
-import { ToggleDrawer } from "./types"
-
-export const toggleDrawer = (payload: boolean): ToggleDrawer => ({
-  type: ActionTypes.TOGGLE_DRAWER,
-  payload
-})
