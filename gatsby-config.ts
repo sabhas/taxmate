7211e15b1b8deb3b72283a6932a33038970e954b
Sabir Hassan <sabirbk06@gmail.com>
2024-06-25 21:20:01 +0500

    add react helmet and gatsby-plugin-manifest

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -7,13 +7,17 @@ require("dotenv").config({
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Taxmate`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: "https://taxmate.pk",
+    description: "",
+    author: "Sabir Hassan",
+    image: "/taxmate.jpg"
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
+    "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
     "gatsby-plugin-sitemap",
@@ -26,6 +30,14 @@ const config: GatsbyConfig = {
         path: "./src/images/"
       },
       __key: "images"
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Taxmate",
+        start_url: "/",
+        icon: "static/taxmate.jpg" // Path to your favicon file
+      }
     }
   ]
 }
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -3,10 +3,43 @@ import { Header } from "../components/Header"
 import { Box } from "@mui/material"
 import { WhatsAppFloatingButton } from "../components/FloatingWhatsappButton"
 import * as styles from "./style.module.scss"
+import { Helmet } from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
 
 const Layout = ({ children }: PropsWithChildren) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          siteUrl
+          description
+          author
+          image
+        }
+      }
+    }
+  `)
+
+  const { title, siteUrl, description, author, image } = data.site.siteMetadata
+
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="author" content={author} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={`${siteUrl}${image}`} />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content={author} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={`${siteUrl}${image}`} />
+      </Helmet>
       <Header />
       <Box className={styles.main}>
         {children}
