cbedc4ac72e21740aae34984e6356ab101e92068
Sabir Hassan <sabirbk06@gmail.com>
2024-07-17 19:12:31 +0500

    chore: quick fix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,16 @@
-import { Menu as MenuIcon } from "@mui/icons-material"
-import { AppBar, Box, Button, IconButton, Toolbar } from "@mui/material"
-import React, { useState } from "react"
-import { Sidebar } from "../Sidebar"
-import * as styles from "./style.module.scss"
-import { StaticImage } from "gatsby-plugin-image"
-import { Link, navigate } from "gatsby"
+import { Menu as MenuIcon } from '@mui/icons-material'
+import { AppBar, Box, Button, IconButton, Toolbar } from '@mui/material'
+import React, { useState } from 'react'
+import { Sidebar } from '../Sidebar'
+import * as styles from './style.module.scss'
+import { StaticImage } from 'gatsby-plugin-image'
+import { Link, navigate } from 'gatsby'
 
 export const Header = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
 
   return (
-    <AppBar position="fixed" className={styles.AppBar}>
+    <AppBar position='fixed' className={styles.AppBar}>
       {/*on small screens show side bar only when menu icon is clicked*/}
       {isSidebarOpen && (
         <Sidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
@@ -18,12 +18,12 @@ export const Header = () => {
 
       <Toolbar className={styles.toolbar}>
         <IconButton
-          size="large"
-          edge="start"
-          aria-label="menu"
+          size='large'
+          edge='start'
+          aria-label='menu'
           sx={{
             display: {
-              md: "none"
+              md: 'none'
             }
           }}
           onClick={() => {
@@ -34,32 +34,32 @@ export const Header = () => {
         </IconButton>
 
         <StaticImage
-          src="../../images/logo.png"
-          alt="Logo"
-          placeholder="blurred"
-          layout="fixed"
+          src='../../images/logo.png'
+          alt='Logo'
+          placeholder='blurred'
+          layout='fixed'
           width={160}
           height={60}
-          onClick={() => navigate("/#home")}
+          onClick={() => navigate('/#home')}
         />
 
         <Box
           className={styles.links}
           sx={{
             display: {
-              xs: "none",
-              md: "flex"
+              xs: 'none',
+              md: 'flex'
             }
           }}
         >
-          <NavigationLink label="Home" link="/#home" />
-          <NavigationLink label="Calculator" link="/taxCalculator" />
-          <NavigationLink label="News" link="/news" showStar />
+          <NavigationLink label='Home' link='/#home' />
+          <NavigationLink label='Calculator' link='/taxCalculator' />
+          <NavigationLink label='News' link='/news' showStar />
         </Box>
 
         <Box className={styles.rightSideBox}>
-          <Button variant="contained" onClick={() => navigate("/#contactUs")}>
-            Book Appointment
+          <Button variant='contained' onClick={() => navigate('/#contactUs')}>
+            Contact Us
           </Button>
         </Box>
       </Toolbar>
