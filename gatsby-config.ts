0ca8e374162171ba225cc9cec9e0aed09b2d22eb
Sabir Hassan <sabirbk06@gmail.com>
2024-07-28 12:19:17 +0500

    feat: add google analytics

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -38,6 +38,23 @@ const config: GatsbyConfig = {
         start_url: "/",
         icon: "static/taxmate.jpg" // Path to your favicon file
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-D0GC595M33", // Google Analytics / GA
+          "GT-TWRW9C4K"
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0
+        }
+      }
     }
   ]
 }
